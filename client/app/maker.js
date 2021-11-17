e7d861045ab3f353c7b0c609d836ce295d9a5955
Justin Neft (RIT Student) <jbn5379@rit.edu>
2021-11-17 14:10:56 -0500

    Began working on react components and handlebar pages

---
+++ client/app/maker.js
@@ -0,0 +1,90 @@
+// Banner creations
+const BannerWindow = (props) => {
+    return (
+        <form id="bannerForm"
+            name="bannerForm"
+            action="/summon"
+            method="POST"
+            className="mainForm"
+            >
+            <input id="singleSum" type="button" onClick={handleSummon}>Summon Once</input>
+            <input id="tenfoldSum" type="button" onClick={tenfoldSummon}>Summon Ten Times</input>
+        </form>
+    );
+    
+};
+
+const singleSummon = (e) => { handleSummon(e, 1); };
+const tenfoldSummon = (e) => { handleSummon(e, 10); };
+
+const handleSummon = (e, summonCount) => {
+
+}
+
+// Password changing
+const handlePassChange = (e) => {
+    e.preventDefault();
+
+    if ($("#oldPass").val() == '' || $("#pass").val() == '' || $("#pass2").val() == '') {
+        handleError("All fields are required");
+        return false;
+    }
+
+    if ($("#pass").val() !== $("#pass2").val()) {
+        handleError("Passwords do not match");
+        return false;
+    }
+
+    sendAjax('POST', $("#passChangeForm").attr("action"), $("#passChangeForm").serialize(), redirect);
+    return false;
+};
+
+const PassChangeWindow = (props) => {
+    return (
+        <form id="passChangeForm"
+              name="passChangeForm"
+              onSubmit={handlePassChange}
+              action="/passChange"
+              method="POST"
+              className="mainForm"
+              >
+              <label htmlFor="oldPass">Old password: </label>
+              <input id="oldPass" type="text" name="oldPass" placeholder="old password"/>
+              <label htmlFor="pass">New Password: </label>
+              <input id="pass" type="password" name="pass" placeholder="new password"/>
+              <label htmlFor="pass2">Retype New Password: </label>
+              <input id="pass2" type="password" name="pass2" placeholder="retype new password"/>
+              <input type="hidden" name="_csrf" value={props.csrf}/>
+              <input className="formSubmit" type="submit" value="Change Password" />
+        </form>
+    );
+};
+
+const createPassChangeWindow = (csrf) => {
+    ReactDOM.render(
+        <PassChangeWindow csrf={csrf} />,
+        docuemnt.querySelector("#content")
+    );
+};
+
+const setup = (csrf) => {
+    const changePassButton = document.querySelector("#changePassButton");
+
+    changePassButton.addEventListener("click", (e) =>{
+        e.preventDefault();
+        createPassChangeWindow(csrf);
+        return false;
+    });
+
+    createLoginWindow(csrf); //default view
+};
+
+const getToken = () => {
+    sendAjax('GET', '/getToken', null, (result) => {
+        setup(result.csrfToken);
+    });
+};
+
+$(document).ready(function() {
+    getToken();
+});
